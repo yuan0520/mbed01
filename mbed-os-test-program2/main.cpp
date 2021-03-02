887878f341ee2adb94d7e15a0cc65ca07b4ceb5f
yuan0520 <abcdabcd10211@gmail.com>
2021-03-02 09:18:41 -0800

    add program2

---
+++ mbed-os-test-program2/main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+#include "led.h"
+
+DigitalOut myLED(LED1);
+DigitalOut myLED2(LED3);
+
+void Led(DigitalOut &ledpin);
+
+int main()
+{
+   myLED = 1;
+   myLED2 = 1;
+   while (true)
+   {
+      Led(myLED);
+      Led(myLED2);
+   }
+}
